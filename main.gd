d127a2e92d1071a191a42fe45271d1e5235542b1
Remo Arpagaus <arpagaus.remo@gmail.com>
2025-10-08 20:14:34 +0200

    Fininalzed game mechanics and HUD

--- main.gd
+++ main.gd
@@ -3,20 +3,21 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
-
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func _on_score_timer_timeout() -> void:
 	score = score + 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
@@ -42,7 +43,7 @@ func _on_mob_timer_timeout() -> void:
 	mob.rotation = direction
 	
 	# Zufallige Geschwindigkeit
-	var velocity = Vector2(randf_range(200.0, 300.0), 0.0)
+	var velocity = Vector2(randf_range(150.0, 200.0), 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
 	
 	add_child(mob)
