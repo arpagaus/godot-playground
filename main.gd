cc67b80ede30a146cdeff9f44e8177e6604e0a81
Remo Arpagaus <arpagaus.remo@gmail.com>
2025-10-08 20:45:47 +0200

    Music and sound effects
    Also fine tune the enemy and player size

--- main.gd
+++ main.gd
@@ -9,11 +9,14 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$GameOver.play()
 
 func _on_score_timer_timeout() -> void:
 	score = score + 1
